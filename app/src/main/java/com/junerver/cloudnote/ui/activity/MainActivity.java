1e7ea536fb5e01771f526acb449674e57fdad4c9
Junerver <junerver@gmail.com>
2016-09-06 17:48:47 +0800

    完成了ViewPager+Fragment+TabView的切换； todo： 完成RecyclerView+CardView的列表显示； 完成FloatingActionButton的功能； 完成新建笔记的功能；

--- app/src/main/java/com/junerver/cloudnote/ui/activity/MainActivity.java
+++ app/src/main/java/com/junerver/cloudnote/ui/activity/MainActivity.java
@@ -1,6 +1,10 @@
 package com.junerver.cloudnote.ui.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,9 +14,14 @@ import com.junerver.cloudnote.CloudNoteApp;
 import com.junerver.cloudnote.R;
 import com.junerver.cloudnote.db.entity.Note;
 import com.junerver.cloudnote.db.entity.NoteEntity;
+import com.junerver.cloudnote.ui.fragment.ConfigFragment;
+import com.junerver.cloudnote.ui.fragment.NoteFragment;
+import com.junerver.cloudnote.ui.widget.TabView;
 
 import java.security.acl.NotOwnerException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,15 +33,13 @@ import cn.bmob.v3.listener.QueryListener;
 import cn.bmob.v3.listener.SaveListener;
 
 public class MainActivity extends BaseActivity {
+    private String[] mTitle = {"笔记", "设置"};
+    private int[] mIconSelect = {R.drawable.docker_tab_doc_selected, R.drawable.docker_tab_setting_selected};
+    private int[] mIconNormal = {R.drawable.docker_tab_doc_normal, R.drawable.docker_tab_setting_normal};
+    private ViewPager mViewPager ;
+    private TabView mTabView ;
+    private Map<Integer,Fragment> mFragmentMap ;
 
-    @BindView(R.id.tvUsername)
-    TextView mTvUsername;
-    @BindView(R.id.etTitle)
-    EditText mEtTitle;
-    @BindView(R.id.etContent)
-    EditText mEtContent;
-    @BindView(R.id.btnSubmit)
-    Button mBtnSubmit;
 
     @Override
     protected void initView() {
@@ -47,10 +54,13 @@ public class MainActivity extends BaseActivity {
 //                        .setAction("Action", null).show();
 //            }
 //        });
-        BmobUser bmobUser = BmobUser.getCurrentUser();
-        if (bmobUser != null) {
-            mTvUsername.setText(bmobUser.getUsername());
-        }
+
+        mFragmentMap = new HashMap<>() ;
+        mViewPager = (ViewPager)findViewById(R.id.id_view_pager) ;
+        mViewPager.setOffscreenPageLimit(2);
+        mViewPager.setAdapter(new PageAdapter(getSupportFragmentManager()));
+        mTabView = (TabView)findViewById(R.id.id_tab) ;
+        mTabView.setViewPager(mViewPager);
 
     }
 
@@ -64,56 +74,52 @@ public class MainActivity extends BaseActivity {
 
     }
 
+    private Fragment getFragment(int position){
+        Fragment fragment = mFragmentMap.get(position) ;
+        if(fragment == null){
+            switch (position){
+                case 0:
+                    fragment = new NoteFragment() ;
+                    break ;
+                case 1:
+                    fragment = new ConfigFragment();
+                    break ;
+            }
+            mFragmentMap.put(position,fragment) ;
+        }
+        return fragment ;
+    }
+
     @Override
     protected int getLayoutId() {
         return R.layout.activity_main;
     }
 
+    class PageAdapter extends FragmentPagerAdapter implements TabView.OnItemIconTextSelectListener{
 
-    @OnClick(R.id.btnSubmit)
-    public void onClick() {
-        String title = mEtTitle.getText().toString().trim();
-        String content = mEtContent.getText().toString().trim();
-        String summary;
-        if (content.length() >= 10) {
-            summary = content.substring(0, 9);
-        } else {
-            summary = content;
+        public PageAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public Fragment getItem(int position) {
+            return getFragment(position);
+        }
+        @Override
+        public int[] onIconSelect(int position) {
+            int icon[] = new int[2] ;
+            icon[0] = mIconSelect[position] ;
+            icon[1] = mIconNormal[position] ;
+            return icon;
+        }
+        @Override
+        public String onTextSelect(int position) {
+            return mTitle[position];
         }
 
-
-
-        Note note = new Note();
-        note.setTitle(title);
-        note.setContent(content);
-        note.setSummary(summary);
-        note.save(new SaveListener<String>() {
-            @Override
-            public void done(String objectId, BmobException e) {
-                if(e==null){
-                    BmobQuery<Note> query = new BmobQuery<Note>();
-                    query.getObject(objectId, new QueryListener<Note>() {
-                        @Override
-                        public void done(Note object, BmobException e) {
-                            if(e==null){
-                                NoteEntity noteEntity = new NoteEntity();
-                                noteEntity.setId(new Date().getTime());
-                                noteEntity.setTitle(object.getTitle());
-                                noteEntity.setContent(object.getContent());
-                                noteEntity.setSummary(object.getSummary());
-                                noteEntity.setObjId(object.getObjectId());
-                                noteEntity.setDate(object.getUpdatedAt());
-                                CloudNoteApp.getNoteEntityDao().insert(noteEntity);
-                            }else{
-                                Log.i("bmob","失败："+e.getMessage()+","+e.getErrorCode());
-                            }
-                        }
-
-                    });
-                }else{
-                    Log.i("bmob","失败："+e.getMessage()+","+e.getErrorCode());
-                }
-            }
-        });
+        @Override
+        public int getCount() {
+            return mTitle.length;
+        }
     }
+
 }
