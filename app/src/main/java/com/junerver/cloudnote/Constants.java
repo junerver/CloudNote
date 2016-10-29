051ce5120f7ba504ad20b22937036ab78e1b0c84
junerver <junerver@gmail.com>
2016-10-29 23:02:01 +0800

    重新申请了Bmob云后台，现在软件可以调试了~

--- app/src/main/java/com/junerver/cloudnote/Constants.java
+++ app/src/main/java/com/junerver/cloudnote/Constants.java
@@ -4,7 +4,8 @@ package com.junerver.cloudnote;
  * Created by Junerver on 2016/9/1.
  */
 public class Constants {
-    public static final String APPID = "0864d7f88908062c99fe51c58c65f254";
+    //Bmob云后台APPID
+    public static final String APPID = "fefce5edf036867d6bbddca07d71599a";
     //数据库名称
     public static final String DB_NAME = "cloudnote";
 }
